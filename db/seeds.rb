447126ba1816fdfc97908ddb756da7889a508f12
maurabaptista <maura.mendes.baptista@gmail.com>
2020-01-13 08:42:00 +0100

    added more seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,6 +40,36 @@ restaurants_attributes = [
     address:      '56A Shoreditch High St, London E1 6PQ',
      user_id: User.all.sample.id
     
+  },
+  {
+    name:         'Epicure',
+    address:      '112 Rue Du Faubourg Saint-Honore | Le Bristol Paris, 75008 Paris',
+     user_id: User.all.sample.id
+    
+  },
+  {
+    name:         'Boutary',
+    address:      '25 Rue Mazarine, 75006 Paris',
+     user_id: User.all.sample.id
+    
+  },
+  {
+    name:         'Pianovins',
+    address:      '46 Rue Trousseau, 75011 Paris',
+     user_id: User.all.sample.id
+    
+  },
+  {
+    name:         'Grenache',
+    address:      '12 patio de dom Fradique, Lisbon 1100-624',
+     user_id: User.all.sample.id
+    
+  },
+  {
+    name:         'ALMA Henrique Sa Pessoa',
+    address:      'Rua Anchieta 15 | Chiado, Lisbon 1200-023, Portugal',
+     user_id: User.all.sample.id
+    
   }
 ]
 Restaurant.create!(restaurants_attributes)
